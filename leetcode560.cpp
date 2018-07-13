ec2e57094a36c1d724255fa243c70b6618fa7c2f
CarolineZhu <14307130373@fudan.edu.cn>
2018-07-14 01:50:28 +0800

    E11 2018/7/13

--- leetcode560.cpp
+++ leetcode560.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int subarraySum(vector<int>& nums, int k) {
 	vector<int> s(nums.size());
-	map<int, int> pos;
+	vector<int> pos;
 	int i, cnt = 0;
 	for (i = 0; i < nums.size(); i++) {
 		if(i == 0) s[i] = nums[i];
@@ -14,21 +14,24 @@ int subarraySum(vector<int>& nums, int k) {
 	}
 	for (i = 0; i < s.size(); i++) {
 		if (s[i] == k) cnt++;
-		pos.insert(make_pair(s[i] + k, i));
-		// cout << s[i] + k << " " << i << endl;
+		pos.push_back(s[i] + k);
+		cout << s[i] + k << " " << i << endl;
 	}
-	// cout << endl;
+	// cout << pos.size() << endl;
 	for (i = 0; i < s.size(); i++) {
-		if (pos.find(s[i]) != pos.end())
-			cnt++;
+		for (int j = 0; j < i; j++) {
+			if (pos[j] == s[i]) {
+				cnt++;
+				cout << i << endl;
+			}
+		}
 	}
-	// cout << i << " " << pos[s[i]] << endl;
 	return cnt;
 }
 
 int main() {
-	int arr[] = {1,1,1};
+	int arr[] = {-1,-1,1};
 	vector<int> nums(arr, arr + 3);
-	cout << subarraySum(nums, 2);
+	cout << subarraySum(nums, 1);
 	return 0;
 }
